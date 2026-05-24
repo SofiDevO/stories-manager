4c1076adbb260b97229de01cacf539eaf5951de5
SofiDevO <angiemeow89@gmail.com>
2026-05-24 16:44:21 -0600

    work in progress

--- src/api/routes/stories.routes.ts
+++ src/api/routes/stories.routes.ts
@@ -1,3 +1,1 @@
-import type { Context } from "hono";
-import { D1StoryRepository } from "../../infrastructure/repositories/D1StoryRepository";
-``;
+import { Hono } from "hono";
---
+++ src/infrastructure/repositories/D1StoryRepository.ts
@@ -0,0 +1,23 @@
+import { IStoryRepository } from "../../domain/repositories/IStoryRepository";
+import type { Story } from "../../domain/entities/Story";
+export class D1StoryRepository implements IStoryRepository{
+    constructor(private db :D1Database){}
+
+    async create(story: Story): Promise<void>{
+        await this.db.prepare(
+            'INSERT INTO stories (id, video_url,created_at) VALUES(?,?,?)'
+        ).bind(story.id, story.videoUrl, story.createdAt).run();
+    }
+    async getActiveStories(): Promise<Story[]>{
+        const {results} = await this.db.prepare(
+            "SELECT id, video_url as videURL, created_at as createdAt FROM stories WHERE created_at > datetime('now', '-1 day') ORDER BY created_at ASC"
+        ).all<Story>();
+        return results || [];
+    }
+
+    async deleteExpiredStories(): Promise<void>{
+        await this.db.prepare(
+            "DELETE FROM stories WHERE created_at <= datetime('now','-1 day')"
+        ).run();
+    }
+}
